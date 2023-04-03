9ae1b6d5ea49ff19909b5a10c4b8ed25ddf576af
G3n4r00 <rm551986@gmail.com>
2023-04-02 21:28:39 -0300

    ex4

--- ex4/exercicio4.js
+++ ex4/exercicio4.js
@@ -1,13 +1,13 @@
 let idade = prompt("Insira sua idade: ")
 if(idade <= 12){
-    console.log("Você é uma criança")
+    alert("Você é uma criança")
 }
 else if(idade > 12 && idade <= 18 ){
-    console.log("Você é adolescente")
+    alert("Você é adolescente")
 }
 else if(idade > 18 && idade <= 60){
-    console.log("Você é adulto")
+    alert("Você é adulto")
 }
 else{
-    console.log("Você é idoso")
+    alert("Você é idoso")
 }
